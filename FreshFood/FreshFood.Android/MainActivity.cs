57e931840310cb7f2f5ae0cb1b3e06c1d3126c92
Robert Badea <badea.robert@ymail.com>
2020-01-26 11:17:22 +0200

    Removed front buttons (battery, etc) on android

--- FreshFood/FreshFood.Android/MainActivity.cs
+++ FreshFood/FreshFood.Android/MainActivity.cs
@@ -21,6 +21,18 @@ namespace FreshFood.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            // code from: https://stackoverflow.com/questions/39248138/how-to-hide-bottom-bar-of-android-back-home-in-xamarin-forms
+            //====================================
+            int uiOptions = (int)Window.DecorView.SystemUiVisibility;
+
+            uiOptions |= (int)SystemUiFlags.LowProfile;
+            uiOptions |= (int)SystemUiFlags.Fullscreen;
+            uiOptions |= (int)SystemUiFlags.HideNavigation;
+            uiOptions |= (int)SystemUiFlags.ImmersiveSticky;
+
+            Window.DecorView.SystemUiVisibility = (StatusBarVisibility)uiOptions;
+            //==================================== 
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
