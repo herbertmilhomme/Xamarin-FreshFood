d1a020f95eba258e9fac395dff423827552e62c0
Robert Badea <badea.robert@ymail.com>
2020-01-26 22:21:46 +0200

    Added custom renderer for iconview

---
+++ FreshFood/FreshFood.iOS/Renderer/IconViewRenderer.cs
@@ -0,0 +1,84 @@
+﻿using CoreGraphics;
+using FreshFood.Custom.CustomControls;
+using FreshFood.iOS.Renderer;
+using System.ComponentModel;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRendererAttribute(typeof(IconView), typeof(IconViewRenderer))]
+namespace FreshFood.iOS.Renderer
+{
+    public class IconViewRenderer : ViewRenderer<IconView, UIImageView>
+    {
+        private bool _isDisposed;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_isDisposed)
+            {
+                return;
+            }
+
+            if (disposing && base.Control != null)
+            {
+                UIImage image = base.Control.Image;
+                UIImage uIImage = image;
+                if (image != null)
+                {
+                    uIImage.Dispose();
+                    uIImage = null;
+                }
+            }
+
+            _isDisposed = true;
+            base.Dispose(disposing);
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<IconView> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                UIImageView uIImageView = new UIImageView(CGRect.Empty)
+                {
+                    ContentMode = UIViewContentMode.ScaleAspectFit,
+                    ClipsToBounds = true
+                };
+                SetNativeControl(uIImageView);
+            }
+            if (e.NewElement != null)
+            {
+                SetImage(e.OldElement);
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            if (e.PropertyName == IconView.SourceProperty.PropertyName)
+            {
+                SetImage(null);
+            }
+            else if (e.PropertyName == IconView.ForegroundProperty.PropertyName)
+            {
+                SetImage(null);
+            }
+        }
+
+        private void SetImage(IconView previous = null)
+        {
+            if (previous == null && !string.IsNullOrWhiteSpace(Element.Source))
+            {
+                var uiImage = new UIImage(Element.Source);
+                uiImage = uiImage.ImageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate);
+                Control.TintColor = Element.Foreground.ToUIColor();
+                Control.Image = uiImage;
+                if (!_isDisposed)
+                {
+                    ((IVisualElementController)Element).NativeSizeChanged();
+                }
+            }
+        }
+    }
+}
